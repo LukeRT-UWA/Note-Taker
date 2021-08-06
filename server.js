2d838db62c9c06bb463a5c08c585f7c5038516b1
Luke Thompson <16175583curtin@gmail.com>
2021-08-06 19:32:11 +0800

    add initial route for index.html

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const api = require('./routes/index.js');
 const PORT = process.env.port || 3001;
 const app = express();
+const path = require('path');
+const { userInfo } = require('os');
 
+app.use(express.static('public'))
 
-
-
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 
 
