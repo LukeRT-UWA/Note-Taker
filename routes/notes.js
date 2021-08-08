593aa799175108170ff40aa729a8e37afe9e62fc
Luke Thompson <16175583curtin@gmail.com>
2021-08-08 17:18:47 +0800

    added ability to save note contents

--- routes/notes.js
+++ routes/notes.js
@@ -2,12 +2,39 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 // const { v4: uuidv4 } = require('uuid');
 
-notes.get('/notes', (req, res) => {
+notes.get('/', (req, res) => {
 
  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
-}
+});
 
-
-)
+// POST Route for submitting feedback
+notes.post('/', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to submit feedback`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        // feedback_id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/notes.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
 
 module.exports = notes;
